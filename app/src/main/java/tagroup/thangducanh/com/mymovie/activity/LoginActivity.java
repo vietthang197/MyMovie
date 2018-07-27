cb77ad2986c7a7ee6d760a9f24e7d564bc56a3eb
Le Viet Thang <levietthang1997@gmail.com>
2018-07-27 21:02:22 +0700

    Initial commit

---
+++ app/src/main/java/tagroup/thangducanh/com/mymovie/activity/LoginActivity.java
@@ -0,0 +1,15 @@
+package tagroup.thangducanh.com.mymovie.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import tagroup.thangducanh.com.mymovie.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
